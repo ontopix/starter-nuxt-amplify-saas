dc10dc2c12cbd8f2964e0f6b133da36f07986b66
Albert Puigsech <albert@puigsech.com>
2025-07-19 14:31:44 +0200

    feat(uix): init layer with nuxt ui pro

---
+++ layers/uix/nuxt.config.ts
@@ -0,0 +1,5 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ["@nuxt/ui-pro"],
+  css: ["@starter-nuxt-amplify-saas/uix/assets/css/main.css"]
+})
