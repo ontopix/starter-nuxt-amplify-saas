dd12dd84f0f3152ad46ab476bb0d1a58cec75033
Albert Puigsech <albert@puigsech.com>
2025-07-21 16:44:22 +0200

    feat(saas): implement the nuxt UI pro dashboard boilerplate

---
+++ apps/saas/app/pages/test.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div>
+    Test Page
+  </div>
+  <div>
+    <p>Authenticator</p>
+    <Authenticator
+      state="signin"
+    />
+  </div>
+  <div>
+    <p>Dummy</p>
+    <Dummy />
+  </div>
+</template>
